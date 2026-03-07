2a73cf816fdc6d8912271c0c65f870b0716a4d0a
Dhanwin007 <dhanwing05@gmail.com>
2026-03-07 12:38:37 +0530

    done with TS

--- reactts/vite-project/src/App.tsx
+++ reactts/vite-project/src/App.tsx
@@ -1,9 +1,11 @@
 
 import './App.css'
-import { ChaiCard } from './components/ChaiCard'
+//import { ChaiCard } from './components/ChaiCard'
 import {Counter} from './components/Counter'
 import {ChaiList} from './components/ChiaList'
 import type {Chai} from "./Types/types"
+import { OrderForm } from './components/OrderForm'
+import Card from './components/Card'
  const menu:Chai[] = [{id:1,name:"Masala",price:12},{id:2,name:"Cinamon",price:39},{id:3,name:"greenTea",price:25}]
 function App() {
   
@@ -14,6 +16,16 @@ function App() {
   <h1>Vite + React</h1>
   <ChaiList items={menu}/>
   <Counter />
+  </div>
+  <div>
+    <OrderForm 
+    onSubmit={(order)=>{
+      console.log("placed",order.name,order.cups)
+    }}/>
+  </div>
+  <div>
+    <Card title="masala Chai" 
+    footer={<button>Order Now</button>} />
   </div>
     </>
   )
--- reactts/vite-project/src/components/OrderForm.tsx
+++ reactts/vite-project/src/components/OrderForm.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {  useState } from "react"
+interface OrderFormProps{
+    onSubmit(order:{name:string;cups:number}):void
+}
 
-export function OrderForm() {
+export function OrderForm({onSubmit}:OrderFormProps) {
+    const [name,setName] = useState<string>("Masala");
+    const [cups,setCups]= useState<number>(1);
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>)
+  {
+    e.preventDefault()
+    onSubmit({name,cups});
+  }
   return (
-    <div>OrderForm</div>
+    <form onSubmit={handleSubmit}>
+      <label>Chai Name</label>
+      <input value={name} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setName(e.target.value)}/>
+        <label>cups</label>
+        <input 
+        type="number"
+        value={cups} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setCups(Number(e.target.value)||0)}/>
+        <button type="submit">Place order</button>
+    </form>
   )
 }
 
